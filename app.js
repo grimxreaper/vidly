aba1e315929f71299cdcb4858a4afcc7675a762f
Zahra <hizahrazadeh@gmail.com>
2021-02-15 19:22:46 -0500

    validateGenre

--- app.js
+++ app.js
@@ -20,3 +20,15 @@ app.get('/api/genres/:id', (req, res) => {
     if (!genre) return res.status(404).send('The genre was not found.');
     res.send(genre);
 });
+
+app.post('/api/genres', (req, res) => {
+    const { error } = validateGenre(req.body);
+    if (error) return res.status(400).send('Invalid Genre')
+})
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(5).required()
+    };
+    return Joi.validate(genre, schema);
+}
